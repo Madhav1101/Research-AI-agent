99343d031eeee955d689ce8c15d8dfd943573856
m1101 <madhavkalal1101@gmail.com>
2026-01-16 16:46:19 +0530

    Response with Structured Format

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatOpenAI(model="gpt-4o-mini")
-response = llm.invoke("what is SOC Analyst")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("human", "{query}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+chain = prompt | llm | parser
+
+response = chain.invoke({"query": "What is Digital Rights Management in detail?"})
 print(response)
